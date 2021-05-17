fe977c44fb1555881fee043fe9144be189fc8e5b
usernametanish <76635990+usernametanish@users.noreply.github.com>
2021-05-16 19:28:06 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,91 @@
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d")
+
+car1_width = 120;
+car1_height = 70;
+car1_image = "https://clipground.com/images/race-car-top-down-clipart-2.png";
+car1_x = 10
+car1_y = 10;
+
+car2_width = 120;
+car2_height = 70;
+car2_image = "https://www.pinclipart.com/picdir/middle/172-1729824_race-car-flag-clip-art.png";
+car2_x = 10
+car1_y = 100;
+
+background_image = "racing.jpg";
+
+function add() {
+    background_imgTag = new Image();
+    background_imgTag.onload = uploadBackground;
+    background_imgTag.src = background_image;
+
+    car1_imgTag = new Image();
+    car1_imgTag.onload = uploadcar1;
+    car1_imgTag.src = car1_image;
+
+    car2_imgTag = new Image();
+    car2_imgTag.onload = uploadcar2;
+    car2_imgTag.src = car2_image;
+
+}
+
+function uploadBackground() {
+    ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
+}
+
+function uploadcar1() {
+    ctx.drawImage(car1_imgTag, car1_x, car1_y, car1.width, car1.height);
+}
+
+function uploadcar2() {
+    ctx.drawImage(car2_imgTag, car2_x, car2_y, car2.width, car2.height);
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+    keyPressed = e.keyCode;
+    console.log(keyPressed);
+        if (keyPressed == '38') {
+            car1_up();
+            console.log("up arrow key");
+        }
+        if(keyPressed == '40')
+        {
+            car1_down();
+            console.log("down arrow key");
+        }
+        if(keyPressed == '37')
+        {
+            car1_left();
+            console.log("left arrow key");
+        }
+        if(keyPressed == '39')
+        {
+            car1_right();
+            console.log("right arrow key");
+        }
+        if(keyPressed == '87')
+        {
+            car2_up();
+            console.log("key w");
+        }
+        if(keyPressed == '83')
+        {
+            car2_down();
+            console.log("key s");
+        }
+
+        if(keyPressed == '65')
+        {
+            car2_left();
+            console.log("key a");
+        }
+        if(keyPressed == '68')
+        {
+            car2_right();
+            console.log("key d");
+        }
+}
